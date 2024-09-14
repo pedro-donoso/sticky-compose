11d9c0050d7adc89f5ccf56e399d911281876d69
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-14 14:03:21 -0300

    cambio tamaño texto

--- app/src/main/java/com/example/sticky/MainActivity.kt
+++ app/src/main/java/com/example/sticky/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.sticky
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,11 @@ class MainActivity : ComponentActivity() {
                 Screen {
                     LazyColumn {
                         items(countries) { country ->
-                            Text(text = country)
+                            Text(
+                                text = country,
+                                modifier = Modifier.padding(16.dp),
+                                style = MaterialTheme.typography.bodyLarge
+                                )
                         }
                     }
                 }
