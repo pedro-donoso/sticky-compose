8d74ee5b93199400497f548901fac1e7e4c55fd9
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-14 13:55:38 -0300

    agrego screen

--- app/src/main/java/com/example/sticky/MainActivity.kt
+++ app/src/main/java/com/example/sticky/MainActivity.kt
@@ -3,27 +3,17 @@ package com.example.sticky
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sticky.ui.theme.StickyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            StickyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme() {
+                Screen {
+
                 }
             }
         }
@@ -31,17 +21,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StickyTheme {
-        Greeting("Android")
+fun Screen(content: @Composable () -> Unit) {
+    MaterialTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            content()
+        }
     }
 }
+
+
+
+
+
